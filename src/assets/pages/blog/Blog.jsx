7c2a88b7b95f7f0237ec293177fa73fd59490adb
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-01-22 16:02:06 +0100

    map update

--- src/assets/pages/blog/Blog.jsx
+++ src/assets/pages/blog/Blog.jsx
@@ -1,7 +1,21 @@
+import blogData from "../../data/main.js"
 const Blog = () => {
+
+    console.log(blogData);
     return (<>
 
         <h5>Das ist die Blog Page</h5>
+        {blogData.map((postare, index) => {
+            return (
+                <div key={index}>
+                    <h5>{postare.title}</h5>
+                    
+
+                </div>
+            )
+        })}
+
+
     </>);
 }
 
